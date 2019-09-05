2247f331a4c32e5b61820118ca49dc4bc970a007
fyypumpkin <fuyaoyao@youzan.com>
2019-09-05 11:50:48 +0800

    test

---
+++ loadbalance/src/main/java/top/fyypumpkin/common/readme/md.java
@@ -0,0 +1,10 @@
+package top.fyypumpkin.common.readme;
+
+/**
+ * @author fyypumpkin on 9/5/19
+ */
+public class md {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
